080d8122735b8d97cca1966052a899409355f859
Calla Rudolph and Max Scher <max@>
2017-08-09 10:49:01 -0700

    refactor routing to utilize edit-post route and remove post-detail component

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('admin');
-  this.route('edit-post');
+  this.route('edit-post', {path: '/edit-post/:post_id'});
 });
 
 export default Router;
