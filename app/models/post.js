7313e89f4d4e6fe38d22e37d57732590563aecca
Calla Rudolph and Max Scher <max@>
2017-08-10 10:43:21 -0700

    add comment-tile to facilitate addNewComment functionality

--- app/models/post.js
+++ app/models/post.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  body: DS.attr()
+  body: DS.attr(),
+  comments: DS.hasMany('comment', { async: true })
 });
--- app/routes/read-post.js
+++ app/routes/read-post.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    saveComment(params) {
+      var newComment = this.store.createRecord('comment', params);
+      var post = params.post;
+      post.get('comments').addObject(newComment);
+      newComment.save().then(function() {
+        return post.save();
+      });
+      this.transitionTo('read-post', post);
+    }
   }
 });
